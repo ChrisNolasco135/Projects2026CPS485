3b3daba9bc455efce59419a6e06c41e079854bc3
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-02-16 20:23:27 -0500

    Fixed db url

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database URL for SQLite.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./backend/users.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
 
 # Create the SQLAlchemy engine to interact with the database
 # connect_args={"check_same_thread": False} is required for SQLite to allow multiple threads to access the database.
