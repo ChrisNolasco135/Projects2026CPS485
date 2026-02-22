45c0cdc98405820a487e2e7b0aa3c0effe145ba7
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-02-22 14:25:35 -0500

    Added model for Database linked to User model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, JSON
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
@@ -21,3 +22,19 @@ class User(Base):
     
     # User status
     is_active = Column(Boolean, default=True)
+
+    # Relationship to user's databases
+    databases = relationship("Database", back_populates="owner")
+
+class Database(Base):
+    """
+    SQLAlchemy model for user databases.
+    """
+    __tablename__ = "databases"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    content = Column(JSON, default={}) # Stores tables, rows, columns as JSON
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="databases")
